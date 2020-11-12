b919423a1bbc8f6333c3cd8068e19f5a6b2e26ad
Miranda Li <mirandamelodies99@Mirandas-MBP-4.lan>
2020-11-11 22:33:29 -0500

    buttons successfully wired; deleted unnecessary garbage

--- js/zine.js
+++ js/zine.js
@@ -7,17 +7,7 @@ const edit = document.querySelector('#edit'),
 
 let curr_page = 1;
 
-// const zine_pages_dict = readJSON('zine_pages.js');
-// console.log(zine_pages_dict);
-
-// var request = new XMLHttpRequest();
-// request.open("GET", "zine_pages.json", false);
-// request.send(null)
-// var zine_pages_dict = JSON.parse(request.responseText);
-
 import {zine_pages_dict} from './zine_pages.js';
-// const pages = zine_pages_dict;
-console.log(zine_pages_dict[2]); // TEST!!
 
 prev_page_button.addEventListener('click', previous_page);
 next_page_button.addEventListener('click', next_page);
