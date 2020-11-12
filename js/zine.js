9d1be6d9ff94a68ebc158f32e10a4701f4cdce3a
Miranda Li <mirandamelodies99@Mirandas-MBP-4.lan>
2020-11-11 22:43:37 -0500

    added page update functionality

--- js/zine.js
+++ js/zine.js
@@ -5,6 +5,7 @@ const edit = document.querySelector('#edit'),
   prev_page_button = document.querySelector('#previous-page'),
   next_page_button = document.querySelector('#next-page');
 
+const num_pages = 8;
 let curr_page = 1;
 
 import {zine_pages_dict} from './zine_pages.js';
@@ -13,20 +14,27 @@ prev_page_button.addEventListener('click', previous_page);
 next_page_button.addEventListener('click', next_page);
 
 function previous_page() {
-  console.log('previous_page triggered')
-  curr_page = (curr_page == 1) ? 8 : (curr_page - 1) % 8;
-  console.log(curr_page);
-  console.log(zine_pages_dict[curr_page]);
-
+  // console.log('previous_page triggered')
+  curr_page = (curr_page == 1) ? num_pages : (curr_page - 1) % num_pages;
+  // console.log(curr_page);
+  // console.log(zine_pages_dict[curr_page]);
+  update_page(curr_page);
 }
 
 function next_page() {
-  console.log('next_page triggered');
-  curr_page = (curr_page == 7) ? 8 : (curr_page + 1) % 8;
-  console.log(curr_page);
-  console.log(zine_pages_dict[curr_page]);
+  // console.log('next_page triggered');
+  curr_page = (curr_page == num_pages - 1) ? num_pages : (curr_page + 1) % num_pages;
+  // console.log(curr_page);
+  // console.log(zine_pages_dict[curr_page]);
+  update_page(curr_page);
 }
 
 function update_page(page_num) {
   console.log('update_page');
+  page = zine_pages_dict[page_num];
+  attrib.innerHTML = page['attrib_text'];
+  gen.setAttribute('src', page['gen_href']);
+  page_number.innerText = page_num;
+  edit.setAttribute('src', page['edit_href']);
+  edit.setAttribute('alt', page['alt_text']);
 }
