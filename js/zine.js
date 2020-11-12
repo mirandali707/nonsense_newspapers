a653c50186413051fcf23de20798037e54a0c866
Miranda Li <mirandamelodies99@Mirandas-MBP-4.lan>
2020-11-11 22:24:46 -0500

    more button stuff

--- js/zine.js
+++ js/zine.js
@@ -23,10 +23,17 @@ next_page_button.addEventListener('click', next_page);
 
 function previous_page() {
   console.log('previous_page triggered')
+  curr_page = (curr_page == 1) ? 8 : (curr_page - 1) % 8;
+  console.log(curr_page)
+  console.log(zine_pages_dict[curr_page])
+
 }
 
 function next_page() {
   console.log('next_page triggered')
+  curr_page = (curr_page == 7) ? 8 : (curr_page + 1) % 8;
+  console.log(curr_page)
+  console.log(zine_pages_dict[curr_page])
 }
 
 function update_page(page_num) {
