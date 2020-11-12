73a91f5bdb6a496c9d3a1a3ae46cc76c20e533a6
Miranda Li <mirandamelodies99@Mirandas-MBP-4.lan>
2020-11-11 22:29:28 -0500

    wow i need a linter

--- js/zine.js
+++ js/zine.js
@@ -25,18 +25,18 @@ next_page_button.addEventListener('click', next_page);
 function previous_page() {
   console.log('previous_page triggered')
   curr_page = (curr_page == 1) ? 8 : (curr_page - 1) % 8;
-  console.log(curr_page)
-  console.log(zine_pages_dict[curr_page])
+  console.log(curr_page);
+  console.log(zine_pages_dict[curr_page]);
 
 }
 
 function next_page() {
-  console.log('next_page triggered')
+  console.log('next_page triggered');
   curr_page = (curr_page == 7) ? 8 : (curr_page + 1) % 8;
-  console.log(curr_page)
-  console.log(zine_pages_dict[curr_page])
+  console.log(curr_page);
+  console.log(zine_pages_dict[curr_page]);
 }
 
 function update_page(page_num) {
-  console.log('update_page')
+  console.log('update_page');
 }
